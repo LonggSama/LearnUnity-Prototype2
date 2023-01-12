6bce3f49ca90164abdb4fa52752f6dd1315d4d10
Long Nguy3n <longgnguyenn9300@gmail.com>
2023-01-13 02:39:46 +0700

    Move Player with left/right Boundary

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] GameObject _player;
+    public float playerSpeed;
+    private float horizontalInput;
+    public float xRange = 15.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xRange = 15f;
+        playerSpeed = 15f; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_player.transform.position.x > xRange)
+        {
+            _player.transform.position = new Vector3(xRange, _player.transform.position.y, _player.transform.position.z);
+        }
+
+        if (_player.transform.position.x < -xRange)
+        {
+            _player.transform.position = new Vector3(-xRange, _player.transform.position.y, _player.transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        _player.transform.Translate(Vector3.right * horizontalInput * playerSpeed * Time.deltaTime);
+    }
+}
